2ae84e6568a75e788cbf1042eea47ad95cda689c
lennascent <dancarlodelaraa@gmail.com>
2024-06-08 12:17:16 +0800

    BeautyBazaar

---
+++ E-commerce/index.js
@@ -0,0 +1,12 @@
+document.getElementById('productsLink').addEventListener('click', function(event) {
+    document.getElementById('productsSection').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('unaabt').addEventListener('click', function(event) {
+    document.getElementById('abtus').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('socials').addEventListener('click', function(event) {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
+
